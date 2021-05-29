3514776854eed71e2f110fb4da7de87dfa55e6b6
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2021-05-30 09:52:45 +1200

    Feature Question-system-for-lecture: Added failing scenario

---
+++ src/test/java/nz/ac/auckland/se754/web/stepdefs/QuestionSystemStepDefinitions.java
@@ -0,0 +1,99 @@
+package nz.ac.auckland.se754.web.stepdefs;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class QuestionSystemStepDefinitions {
+
+    @Given("Lecturer receives question {string}")
+    public void lecturer_receives_question(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Lecturer presses view question button")
+    public void lecturer_presses_view_question_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Lecturer should view {string}")
+    public void lecturer_should_view(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Lecturer receives anonymous question as {string}")
+    public void lecturer_receives_anonymous_question_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Lecturer presses the mark button")
+    public void lecturer_presses_the_mark_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Lecturer sees question {string}")
+    public void lecturer_sees_question(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Lecturer sees question as marked {string}")
+    public void lecturer_sees_question_as_marked(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Lecturer presses the flag button")
+    public void lecturer_presses_the_flag_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Lecturer sees question as flagged {string}")
+    public void lecturer_sees_question_as_flagged(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Lecturer presses the delete button")
+    public void lecturer_presses_the_delete_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Lecturer should not see the question {string}")
+    public void lecturer_should_not_see_the_question(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Lecturer presses the store button")
+    public void lecturer_presses_the_store_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Lecturer sees question as stored {string}")
+    public void lecturer_sees_question_as_stored(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Lecturer opens Question Database")
+    public void lecturer_opens_question_database() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Lecturer presses the mark as read button")
+    public void lecturer_presses_the_mark_as_read_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- src/test/resources/features/question-system-during-lecture.feature
+++ src/test/resources/features/question-system-during-lecture.feature
@@ -1,47 +1,10 @@
-Feature: Lecturer Views Question
+@Question-System
+Feature: Question System during Lecture
+
+  Background:
+    Given Lecturer receives question "Can you explain interactivity?"
+    When Lecturer presses view question button
 
   Scenario: Lecturer receives anonymous question
-    Given Lecturer should receive "Can you explain interactivity?"
-    When Lecturer presses view question button
     Then Lecturer should view "Can you explain interactivity?"
-    And Lecturer receives anonymous question as "true"
-
-  Scenario: Lecturer marks question
-    Given Lecturer views question "Can you explain interactivity?"
-    When Lecturer presses the mark button
-    Then Lecturer sees question "Can you explain interactivity?"
-    And Lecturer sees question as marked "true"
-
-  Scenario: Lecturer flags question
-    Given Lecturer views question "Can you explain interactivity?"
-    When Lecturer presses the flag button
-    Then Lecturer sees question "Can you explain interactivity?"
-    And Lecturer sees question as flagged "true"
-
-  Scenario: Lecturer deletes question
-    Given Lecturer views question "Can you explain interactivity?"
-    When Lecturer presses the delete button
-    Then Lecturer should not see the question "Can you explain interactivity?"
-
-
-  Scenario: Lecturer flags and deletes question
-    Given Lecturer views question "Can you explain interactivity?"
-    When Lecturer presses the flag button
-    Then Lecturer sees question "Can you explain interactivity?"
-    And Lecturer sees question as flagged "true"
-    When Lecturer presses the delete button
-    Then Lecturer should not see the question "Can you explain interactivity?"
-
-  Scenario: Lecturer stores question
-    Given Lecturer views question "Can you explain interactivity?"
-    When Lecturer presses the store button
-    Then Lecturer sees question "Can you explain interactivity?"
-    And Lecturer sees question as stored "true"
-    When Lecturer opens Question Database
-    Then Lecturer sees question "Can you explain interactivity?"
-
-  Scenario: Lecturer flags question
-    Given Lecturer views question "Can you explain interactivity?"
-    When Lecturer presses the mark as read button
-    Then Lecturer sees question "Can you explain interactivity?"
-    And Lecturer sees question as marked "true"
+    And Lecturer receives anonymous question as "Can you explain interactivity?"
